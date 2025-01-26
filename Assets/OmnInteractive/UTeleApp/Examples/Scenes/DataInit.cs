05a62dd2102343126e14716e8a5aa36bd6558adf
Ruslan <lelyimalikrusik12301@gmail.com>
2025-01-26 22:57:16 +0300

    вфы

---
+++ Assets/OmnInteractive/UTeleApp/Examples/Scenes/DataInit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UTeleApp;
+using UnityEngine.UI;
+
+public class DataInit : MonoBehaviour
+{
+    public Text _initDataText;
+
+    void Start()
+    {
+
+
+    }
+
+    public void TestAlertInit()
+    {
+        
+    }
+
+}
